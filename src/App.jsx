fe348c5584b28131858471f5d4fac959ea09adcf
diegohmad <diego.diniz@estudante.iftm.edu.br>
2024-07-09 22:28:26 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Apresentacao from './components/Apresentacao.jsx';
+import './css/styles.css'
+
+function App() {
+  return (
+    <>
+      <Apresentacao />
+    </>
+  );
+}
+
+export default App;
